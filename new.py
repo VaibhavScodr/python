7de7ddc29e97f821a8e6a5c0064e15ac3068b2b9
vaibhav <cs2021183@gobal.org.in>
2024-10-06 11:18:24 +0530

    write code

--- new.py
+++ new.py
@@ -1,1 +1,9 @@
-print('velidation')
+import re
+#print('velidation')
+email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email=input('enter the mail : ')
+
+if re.search(email_condition,user_email):
+     print("right email")
+else:
+    print('wrong mail')
